b9af5d5847308d6b8041ca060717d7b6687cb6ad
Tobias <112574702+ta223dh@users.noreply.github.com>
2025-11-17 09:53:12 +0100

    #11 feat: implemented root welcome message

--- src/main/java/com/example/todo/controller/RootController.java
+++ src/main/java/com/example/todo/controller/RootController.java
@@ -1,5 +1,13 @@
 package com.example.todo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RootController {
-  
+
+  @GetMapping("/api")
+  public String welcome() {
+    return "Welcome to the Todo REST API";
+  }
 }
