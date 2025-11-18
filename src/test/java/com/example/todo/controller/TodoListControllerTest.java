2c919071ec18806220a115f7494cd25bdf3af85a
Tobias <112574702+ta223dh@users.noreply.github.com>
2025-11-18 19:00:14 +0100

    #2 add unit tests for POST /lists

--- src/test/java/com/example/todo/controller/TodoListControllerTest.java
+++ src/test/java/com/example/todo/controller/TodoListControllerTest.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import com.example.todo.dto.TodoListRequest;
 import com.example.todo.dto.TodoListResponse;
 import com.example.todo.service.TodoListService;
 
@@ -47,5 +49,43 @@ public class TodoListControllerTest {
     .andExpect(jsonPath("$[0].name").value("My first todo list"))
     .andExpect(jsonPath("$[1].id").value(2))
     .andExpect(jsonPath("$[1].name").value("My second todo list"));
+  }
+
+  @Test
+  void shouldReturnCreatedTDO_whenPostingNewTodoList() throws Exception {
+    final String TODO_LIST_NAME = "My new todo list";
+    var requestBody = """
+      {
+        "name": "%s"
+      }
+      """.formatted(TODO_LIST_NAME);
+
+    when(todoListService.createList(new TodoListRequest(TODO_LIST_NAME)))
+      .thenReturn(new TodoListResponse(1, TODO_LIST_NAME));
+
+    mockMvc.perform(
+        post("/api/lists")
+          .contentType("application/json")
+          .content(requestBody)
+      )
+      .andExpect(status().isCreated())
+      .andExpect(jsonPath("$.id").value(1))
+      .andExpect(jsonPath("$.name").value(TODO_LIST_NAME));
+  }
+
+  @Test
+  void shouldReturnBadRequest_whenPostingNewTodoListWithEmptyName() throws Exception {
+    var requestBody = """
+      {
+        "name": ""
+      }
+      """;
+
+    mockMvc.perform(
+          post("/api/lists")
+          .contentType("application/json")
+          .content(requestBody)
+      )
+      .andExpect(status().isBadRequest());
   }
 }
