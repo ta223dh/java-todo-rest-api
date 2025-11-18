3c113ae1a140f6922fe674991d920d1a8d7ef611
Tobias <112574702+ta223dh@users.noreply.github.com>
2025-11-18 17:33:03 +0100

    #1 implement basic GET /lists (no pagination) by having TodoListService fetch data from TodoListRepository

--- src/main/java/com/example/todo/service/TodoListService.java
+++ src/main/java/com/example/todo/service/TodoListService.java
@@ -5,11 +5,22 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.TodoListDto;
 
+import com.example.todo.repository.TodoListRepository;
+
 @Service
 public class TodoListService {
 
+  private final TodoListRepository todoListRepository;
+
+  public TodoListService(TodoListRepository todoListRepository) {
+    this.todoListRepository = todoListRepository;
+  }
+
   public List<TodoListDto> getAllLists() {
 
-    return List.of();
+    return this.todoListRepository.findAll()
+        .stream()
+        .map(entity -> new TodoListDto(entity.getId(), entity.getName()))
+        .toList();
   }
 }
